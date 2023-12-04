ec58a452a662c9e98d1abd05c720a80f125b32fa
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2023-12-04 14:47:43 -0300

    Modificações dos HTML's para php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$Dbserver = 'local-host';
+$Dbusername = 'root';
+$Dbpassword = '';
+$Dbname = 'DbForms';
+
+$conexao = new mysqli($Dbserver, $Dbusername, $Dbpassword, $Dbname);
+
+?>
--- forms.php
+++ forms.php
@@ -13,39 +13,39 @@
 <body>
     <main>
         <div class="w-50 h-50 box-maior">
-            <form class="row" action="forms.html" method="post">
+            <form class="row" action="forms.php" method="post">
                 <h2 class="titulo-forms">Formulario de Cadastro de Aluno</h2>
                 <div class="col-md-12">
                     <label class="form-label">Nome Completo</label>
                     <input type="text" name="nome" class="form-control" id="inputEmail4">
                 </div>
                 <div class="col-md-6">
+                    <label class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control" id="inputCity">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Telefone</label>
+                    <input type="text" name="telefone"  class="form-control" >
+                </div>
+                <div class="col-md-6">
                     <label class="form-label">CEP</label>
                     <input type="text" name="cep" class="form-control">
                 </div>
                 <div class="col-md-6">
-                    <label class="form-label">Telefone</label>
-                    <input type="text" class="form-control" name="telefone" >
+                    <label class="form-label">Estado</label>
+                    <input type="text" name="estado" class="form-control">
                 </div>
                 <div class="col-12">
                     <label class="form-label">Logradouro</label>
-                    <input type="text" class="form-control" name="endereco" placeholder="Ex: Rua X, 13">
+                    <input type="text" name="endereco" class="form-control" placeholder="Ex: Rua X, 13">
                 </div>
                 <div class="col-md-6">
                     <label class="form-label">Cidade</label>
                     <input type="text" name="cidade" class="form-control">
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label">Estado</label>
-                    <input type="text" class="form-control" name="estado" >
-                </div>
+                </div> 
                 <div class="col-md-6">
                     <label class="form-label">Data de Nascimento</label>
-                    <input type="date" class="form-control" name="data-nascimento" >
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" id="inputCity">
+                    <input type="date" name="data-nascimento" class="form-control" name="data-nascimento" >
                 </div>
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Escolaridade</label>
@@ -59,12 +59,12 @@
                 </div>
                 <div class="col-md-6">
                     <label class="form-label">Instituição Anterior</label>
-                    <input type="text" class="form-control" id="inputPassword4">
+                    <input type="text" name="instituicao-anterior" class="form-control" id="inputPassword4">
                 </div>
                 <hr>
 
                 <div class="col-12 mt-3">
-                  <button type="submit" class="btn btn-outline-light position-absolute top-30 start-50 translate-middle">Cadastrar</button>
+                  <button type="submit" name="submit" class="btn btn-outline-light position-absolute top-30 start-50 translate-middle">Cadastrar</button>
                 </form>
             </div>
             <script src="app.js"></script>
